9dbd12d1edf56ea8ad337757128a52f397475347
shia-xia <69449118+shia-xia@users.noreply.github.com>
2023-02-15 13:10:56 +0800

    第一次提交

--- README.md
+++
@@ -1,4 +0,0 @@
-# java17-2023-se-sourceCode
-尚硅谷2023java基础-jdk17-idea2022
-
-## 个人课后代码汇总，个人学习使用。
---
+++ java_SE_1/src/变量/ArrayListModule.java
@@ -0,0 +1,18 @@
+package 变量;
+
+/**
+ * @Description TODO
+ * @Author XJY
+ * @Date 2023/2/15 13:09
+ **/
+public class ArrayListModule {
+    public static void main(String[] args) {
+        ArrayListModule a = new ArrayListModule();
+        a.test();
+    }
+
+    public void test() {
+        ArrayListModule a = new ArrayListModule();
+        a.test();
+    }
+}
